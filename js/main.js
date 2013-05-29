978f459994bb135d3f163369b981ce3fa0e9a726
Joe Madden <joe@beardbro.com>
2013-05-29 17:43:13 -0400

    pushing this shit

--- js/main.js
+++ js/main.js
@@ -2,16 +2,44 @@ var storiesFunction = function ($) {
 
     var storyNav = $('.storyHole li');
     var stories = $('.stories');
+    var showStories = $('.showStories');
+    var inspireNode = $('.page-node-305');
 
-    storyNav.hover(function () {
-        var that = $(this);
-        stories.hide();
-        storyNav.removeClass('active');
-        that.addClass('active');
-        $('.' + that.attr('data-matchingStory')).show();
-    },
-    function () {
+    if ( inspireNode.length ) {
+        alert('it');
+        $(window).scroll(function() {
+            console.log('scrolling');
+        });
 
-    });
+        storyNav.click(function () {
+            var that = $(this);
+            stories.hide();
+            storyNav.removeClass('active');
+            that.addClass('active');
+            $('.' + that.attr('data-matchingStory')).show();
+        });
+
+        showStories.click(function() {
+            var that = $(this);
+            var storiesShown = that.hasClass('on');
+            var storyWrap = $('.storyHole');
+            var groupBg = $('.groupBg');
+
+            if ( storiesShown ) {
+                that.removeClass('on').addClass('off');
+                storyWrap.hide(0);
+                groupBg.show(0);
+            } else {
+                that.removeClass('off').addClass('on');
+                storyWrap.show(0);
+                groupBg.hide(0);
+            }
+
+            return false;
+        });
+
+    }
+
+
 
 }(jQuery);
